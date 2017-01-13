73ba334eb39731ef36957b8df287914e08929777
Kore Nordmann <mail@kore-nordmann.de>
2017-01-13 10:53:06 +0100

    Added README

---
+++ README.md
@@ -0,0 +1,15 @@
+# Minimal Symfony
+
+This is an example repository with a minimized Symfony 3 installation.
+
+## Usage
+
+First run:
+
+    composer install
+
+Afterwards you can "use" this by executing:
+
+    php -S localhost:8000 -t web/ web/index.php
+
+The open http://localhost:8000/ in your web browser.
--- web/index.php
+++ web/index.php
@@ -5,6 +5,14 @@ use Symfony\Component\HttpFoundation\Request;
 $loader = require __DIR__.'/../app/autoload.php';
 include_once __DIR__.'/../var/bootstrap.php.cache';
 
+// @TODO: Remove if set up with anything but PHPs internal webserver
+if (file_exists(__DIR__ . $_SERVER['REQUEST_URI']) &&
+    is_file(__DIR__ . $_SERVER['REQUEST_URI'])) {
+    // Do not try to server static files – this is only important, if used
+    // together with PHPs internal webserver.
+    return false;
+}
+
 // @TODO: Read environment and debug from config
 $kernel = new AppKernel('dev', true);
 $kernel->loadClassCache();
